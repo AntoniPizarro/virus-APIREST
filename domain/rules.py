20d0baef064d1893ba327836b841dcad4803601c
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-04-18 22:02:37 +0200

    feat: Cambio de cuerpo
    Se ha añadido la fiuncionalidad de cambiar de cuerpo entre dos jugadores

--- domain/rules.py
+++ domain/rules.py
@@ -2,6 +2,9 @@ class Rules:
 
     @staticmethod
     def check_body(body):
+        '''
+        Comprueba que un cuerpo tiene 4 cartas sanas para ganar la partida
+        '''
         organs = 0
         
         for i in range(5):
@@ -17,3 +20,37 @@ class Rules:
             return True
         else:
             return False
+    
+    @staticmethod
+    def change_body(player_A, player_B):
+        '''
+        Cambia el cuerpo entre dos jugadores
+        '''
+        new_player_A = {
+            "id" : player_A["id"],
+            "name" : player_A["name"],
+            "body" : player_A["body"],
+            "mallet" : player_A["mallet"]
+        }
+        new_player_B = {
+            "id" : player_B["id"],
+            "name" : player_B["name"],
+            "body" : player_B["body"],
+            "mallet" : player_B["mallet"]
+        }
+
+        comodin_body = new_player_A["body"]
+        
+        new_player_A["body"] = new_player_B["body"]
+        new_player_B["body"] = comodin_body
+        
+        return [new_player_A, new_player_B]
+    
+    @staticmethod
+    def change_organ(body_A, body_b, organ_A, organ_B):
+        comodin = organ_A
+        for organ_body_A in body_A["organs"]:
+            if organ_body_A["organ"] == organ_A:
+                for organ_body_B in body_b["organs"]:
+                    if organ_body_B["organ"] == organ_B:
+                        organ_body_B["organ"] = comodin
--- repository/models.py
+++ repository/models.py
@@ -10,33 +10,44 @@ class Cards(Document):
         return {"name": self.name, "type": self.type, "color": self.color}
 
 class User_list(Document):
+    id = StringField(required=True)
     name = StringField(required=True)
-    deck = ListField(required=True)
+    mallet = ListField(required=True)
     body = DictField(required=True)
 
     def to_json(self):
-        return {"name": self.name, "deck": self.deck, "body": self.body}
+        return {"id" : self.id, "name": self.name, "mallet": self.mallet, "body": self.body}
 
 '''
 Definiendo estructura del body de cada usuario
 
 {
-    "organ1" : {},
-    "organ2" : {},
-    "organ3" : {},
-    "organ4" : {},
-    "organ5" : {},
-
-    "effect1" : {},
-    "effect2" : {},
-    "effect3" : {},
-    "effect4" : {},
-    "effect5" : {},
-
-    "inmune1" : False,
-    "inmune2" : False,
-    "inmune3" : False,
-    "inmune4" : False,
-    "inmune5" : False
+    "organs" : {
+        "organ1" : {
+            "organ" : {},
+            "effect" : {},
+            "inmune" : False/True
+        },
+        "organ2" : {
+            "organ" : {},
+            "effect" : {},
+            "inmune" : False/True
+        },
+        "organ3" : {
+            "organ" : {},
+            "effect" : {},
+            "inmune" : False/True
+        },
+        "organ4" : {
+            "organ" : {},
+            "effect" : {},
+            "inmune" : False/True
+        },
+        "organ5" : {
+            "organ" : {},
+            "effect" : {},
+            "inmune" : False/True
+        }
+    }
 }
 '''
