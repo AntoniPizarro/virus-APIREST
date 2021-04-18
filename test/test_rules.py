6e7f52e03ce0b1b94f6144b81397b2c9619951aa
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-04-18 22:02:52 +0200

    test: Cambio de cuerpo

--- test/test_rules.py
+++ test/test_rules.py
@@ -48,3 +48,29 @@ def test_check_body_health():
     }
 
     assert True == Rules().check_body(body)
+
+@pytest.mark.rules
+def test_change_body():
+    player_A = {
+        "id" : "pa123",
+        "name" : "name 1",
+        "body" : {
+            "organ1" : "organ 1",
+            "organ2" : "organ 2",
+            "organ3" : "organ 3"
+        },
+        "mallet" : ["virus", "medicine", "organ"]
+    }
+    player_B = {
+        "id" : "pb123",
+        "name" : "name 1",
+        "body" : {
+            "organ1" : "",
+            "organ2" : "organ 2",
+            "organ3" : ""
+        },
+        "mallet" : ["virus", "medicine", "organ"]
+    }
+
+    assert player_A["body"] == Rules().change_body(player_A, player_B)[1]["body"]
+    assert player_B["body"] == Rules().change_body(player_A, player_B)[0]["body"]
