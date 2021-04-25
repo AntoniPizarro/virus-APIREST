5755ddbf6f835839648f9ae31591679b0498daec
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-04-25 19:37:57 +0200

    feat: Rutas API
    Se han añadido algunas rutas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 from flask_cors import CORS
+import json
 
 from services.db import data_base as db
 from services.db_engine import init_app
@@ -20,31 +21,70 @@ def ping():
 
 @app.route("/cards", methods=["GET"])
 def get_cards():
+    '''
+    'Obten todas las cartas de la partida
+    '''
     return db.get_cards(DB, HOST)
+    
+@app.route("/users", methods=["GET"])
+def get_users():
+    '''
+    'Obten el usuario por código
+    '''
+    return db.get_users(DB, HOST)
 
-@app.route("/<string:user>/", methods=["GET"])
-def get_user(user):
-    return jsonify({"user" : user})
+@app.route("/users/<string:code>/", methods=["GET"])
+def get_user(code):
+    '''
+    'Obten el usuario por código
+    '''
+    user = db.get_user(DB, HOST, code)
+    return jsonify({"users" : user})
 
-@app.route("/<string:user>/cards", methods=["GET"])
-def get_user_cards(user):
-    return jsonify({"user" : user, "cards" : "cards list"})
+@app.route("/users/<string:code>/body", methods=["GET"])
+def get_user_body(code):
+    '''
+    'Obten el cuerpo del usuario por código
+    '''
+    user = db.get_user(DB, HOST, code)
+    if user == None:
+        return jsonify({"user" : user})
+    else:
+        return jsonify({"user_name" : user["name"], "user_code" : user["code"], "body" : user["body"]})
 
-@app.route("/<string:user>/body", methods=["GET"])
-def get_user_body(user):
-    return jsonify({"user" : user, "body" : "body cards"})
-
-@app.route("/<string:user>/deck", methods=["GET"])
-def get_user_deck(user):
-    return jsonify({"user" : user, "deck" : "deck cards"})
+@app.route("/users/<string:code>/mallet", methods=["GET"])
+def get_user_mallet(code):
+    '''
+    'Obten el mazo del usuario por código
+    '''
+    user = db.get_user(DB, HOST, code)
+    if user == None:
+        return jsonify({"user" : user})
+    else:
+        return jsonify({"user_name" : user["name"], "user_code" : user["code"], "mallet" : user["mallet"]})
 
 
 # POST
 
-@app.route("/<string:user>/uses/<string:card>", methods=["POST"])
-def use_card(user, card):
-    return jsonify({"user" : user, "uses" : card})
+@app.route("/register_user/<string:user>", methods=["POST"])
+def register_user(user):
+    '''
+    'Registra un nuevo usuario
+    '''
+    db.register_user(DB, HOST, json.loads(user))
+    return redirect("../users", code=302)
+
+@app.route("/users/<string:code>/uses/<string:card>", methods=["POST"]) # Pendiente
+def user_uses_card(code, card):
+    return jsonify({"user" : user, "mallet" : card})
+
+
+# TEST
+@app.route("/test")
+def test_route():
+    return "Ruta de testeo"
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5505)
+    dict = {"code":"0002","name":"Pepe","mallet":["Mazo de Pepe"],"body":{"cuerpo":"de Pepe"}}
